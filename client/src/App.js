954450c00be1e3ee5986496ef077b7df8e9fdc3f
Gauri Surve <34476431+GauriSurve@users.noreply.github.com>
2023-05-09 22:57:30 +0530

    Readding client folder

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, { Component }  from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello
+      </header>
+    </div>
+  );
+}
+
+export default App;
