747f106060eaa320f07e5c1143a7dc633a1622d0
Gauri Surve <34476431+GauriSurve@users.noreply.github.com>
2023-05-09 06:14:39 +0530

    initial commit

---
+++ server/controllers/posts.js
@@ -0,0 +1,12 @@
+import PostMessage from "../models/postMesage.js";
+
+export const getPosts = (req, res) => {
+    res.send('This works!');
+}
+
+export const createPost = (req, res) => {
+    res.send('Post creation!');
+}
+
+
+
